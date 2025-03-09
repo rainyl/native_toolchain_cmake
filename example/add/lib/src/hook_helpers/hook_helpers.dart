a5dd1219dcf56a9236eb107959f3f1293bdc8dfa
MichealReed <micheal.reed@outlook.com>
2025-03-09 11:41:46 -0500

    split build and BuildGit examples

--- example/add/lib/src/hook_helpers/hook_helpers.dart
+++ example/add/lib/src/hook_helpers/hook_helpers.dart
@@ -11,24 +11,56 @@ Future<void> runBuild(
   BuildOutputBuilder output,
   Uri sourceDir,
 ) async {
-  // Local src directory
-  // final builder = CMakeBuilder.create(
-  //   name: name,
-  //   sourceDir: sourceDir,
-  //   defines: {
-  //     'CMAKE_BUILD_TYPE': 'Release',
-  //     'CMAKE_INSTALL_PREFIX': '${input.outputDirectory.toFilePath()}/install',
-  //   },
-  //   targets: [
-  //     'install',
-  //   ],
-  //   buildLocal: true,
-  //   logger: Logger('')
-  //     ..level = Level.ALL
-  //     // temp fwd to stderr until process logs pass to stdout
-  //     ..onRecord.listen((record) => stderr.writeln(record)),
-  // );
+  final builder = CMakeBuilder.create(
+    name: name,
+    sourceDir: sourceDir,
+    defines: {
+      'CMAKE_BUILD_TYPE': 'Release',
+      'CMAKE_INSTALL_PREFIX': '${input.outputDirectory.toFilePath()}/install',
+    },
+    targets: [
+      'install',
+    ],
+    buildLocal: true,
+    logger: Logger('')
+      ..level = Level.ALL
+      // temp fwd to stderr until process logs pass to stdout
+      ..onRecord.listen((record) => stderr.writeln(record)),
+  );
 
+  await builder.run(
+    input: input,
+    output: output,
+    logger: Logger('')
+      ..level = Level.ALL
+      ..onRecord.listen((record) => stderr.writeln(record)),
+  );
+
+  final libPath = switch (input.config.code.targetOS) {
+    OS.linux => "install/lib/libadd.so",
+    OS.macOS => "install/lib/libadd.dylib",
+    OS.windows => "install/lib/add.dll",
+    OS.android => "install/lib/libadd.so",
+    OS.iOS => "install/lib/libadd.dylib",
+    _ => throw UnsupportedError("Unsupported OS")
+  };
+  output.assets.code.add(
+    CodeAsset(
+      package: name,
+      name: '$name.dart',
+      linkMode: DynamicLoadingBundled(),
+      os: input.config.code.targetOS,
+      file: input.outputDirectory.resolve(libPath),
+      architecture: input.config.code.targetArchitecture,
+    ),
+  );
+}
+
+Future<void> runBuildGit(
+  BuildInput input,
+  BuildOutputBuilder output,
+  Uri sourceDir,
+) async {
   // From git url
   final builder = CMakeBuilder.fromGit(
     gitUrl: "https://github.com/rainyl/native_toolchain_cmake.git",
